18a786470cb7876d1b9ca08161e0cc52b49de003
Ryan Kelley <ryan.kelley.1223@gmail.com>
2020-12-19 12:45:27 -0500

    all endpoints working, but only without owner

--- Controllers/workoutController.js
+++ Controllers/workoutController.js
@@ -29,63 +29,63 @@ router.post('/newworkout', validateSession, (req, res) => {
             .catch((err) => res.status(500).json({ error: err }))
 
     } else {
-        res.status(403).json({ error: 'User is not authorized to add new workouts.' })
+        res.status(403).json({ error: 'User not authorized to add new workouts.' })
     }
-}) //END ADD NEW BLOG (CREATE)
+}) //END ADD NEW WORKOUT (CREATE)
 
 //READ
-////SEE ALL BLOGS 
-router.get('/allblogs', (req, res) => {
+////SEE ALL WORKOUTS
+router.get('/allworkouts', (req, res) => {
     //validateSession not necessary
 
-    Blog.findAll()
-        .then((blogs) => res.status(200).json(blogs))
+    Workout.findAll()
+        .then((workouts) => res.status(200).json(workouts))
         .catch((err) => res.status(500).json({ error: err }))
-}) //END SEE ALL BLOGS (READ)
+}) //END SEE ALL WORKOUTS (READ)
 
 //UPDATE
-////EDIT BLOG
-router.put('/editblog/:blogId', validateSession, function (req, res) {
+////EDIT WORKOUT
+router.put('/editworkout/:workoutId', validateSession, function (req, res) {
 
     if (req.user.isAdmin == true) {
 
-        const editBlog = {
-            category: req.body.blog.category,
-            title: req.body.blog.title,
-            subtitle: req.body.blog.subtitle,
-            pubDate: req.body.blog.pubDate,
-            contentBody: req.body.blog.contentBody,
-            owner: req.user.id,
+        const revisedWorkoutEntry = {
+            workoutIntention: req.body.workout.workoutIntention,
+            workoutTitle: req.body.workout.workoutTitle,
+            workoutContent: req.body.workout.workoutContent,
+            workoutGuidance: req.body.workout.workoutGuidance,
+            workoutPubDate: req.body.workout.workoutPubDate,
+            workoutOwner: req.user.id,
         }
 
-        const query = { where: { id: req.params.blogId, owner: req.user.id } };
+        const query = { where: { id: req.params.workoutId } }; //owner not working again but .toString() not helping----QQQ
 
-        Blog.update(editBlog, query)
-            .then((blogs) => res.status(200).json(blogs))
+        Workout.update(revisedWorkoutEntry, query)
+            .then((workout) => res.status(200).json(workout))
             .catch((err) => res.status(500).json({ error: err }))
 
     } else {
-        res.status(403).json({ error: 'User not authorized to edit blogs.' })
+        res.status(403).json({ error: 'User not authorized to edit workouts.' })
     }
-}) //END EDIT BLOG (UPDATE)
+}) //END EDIT WORKOUT (UPDATE)
 
 //DELETE
-////DELETE BLOG
-router.delete('/deleteblog/:blogId', validateSession, function (req, res) {
+////DELETE WORKOUT
+router.delete('/deleteworkout/:workoutId', validateSession, function (req, res) {
 
     if (req.user.isAdmin == true) {
 
-        const query = { where: { id: req.params.blogId, owner: req.user.id } }
+        const query = { where: { id: req.params.workoutId } } //owner?----QQQ
         //.toString()--should not need so long as owner is integer
 
-        Blog.destroy(query)
-            .then(() => res.status(200).json({ message: 'Blog has been deleted.' }))
+        Workout.destroy(query)
+            .then(() => res.status(200).json({ message: 'Workout has been deleted.' }))
             .catch((err) => res.status(500).json({ error: err }))
 
     } else {
-        res.status(403).json({ error: 'User not authorized to delete blogs.' })
+        res.status(403).json({ error: 'User not authorized to delete workouts.' })
     }
-}) //END DELETE BLOG (DELETE)
+}) //END DELETE WORKOUT (DELETE)
 
 
 module.exports = router;
